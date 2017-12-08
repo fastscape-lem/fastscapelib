6cc7dfab88c71da05670d7131157f42d6949e68b
Benoit Bovy <benbovy@gmail.com>
2017-12-08 18:28:35 +0100

    add helper script to build test and (re)install

---
+++ build_test_install.sh
@@ -0,0 +1,21 @@
+# Helper script that
+#  - reinstall C++ headers in the activated conda env,
+#  - build and run the test suite (GTest)
+#  - reinstall the Python package in the activated conda env
+#
+# Run this script from the (root) source directory.
+
+ACTIVE_ENV=$(conda info | grep 'default environment' | awk '{print $4}')
+
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$ACTIVE_ENV ..
+make install
+
+cmake -DBUILD_TESTS=ON
+make run_tests
+
+cd ../fastscape-python/
+pip uninstall fastscape --yes
+pip install .
+
+cd ..
