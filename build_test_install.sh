01387df555b2eb90e2546cc4a64c526ad2245051
Benoit Bovy <benbovy@gmail.com>
2017-12-14 12:06:55 +0100

    highlighted messages in build home-made script

--- build_test_install.sh
+++ build_test_install.sh
@@ -4,16 +4,22 @@
 #  - reinstall the Python package in the activated conda env
 #
 # Run this script from the (root) source directory.
+YLW='\033[1;33m'
+NC='\033[0m'
 
 ACTIVE_ENV=$(conda info | grep 'default environment' | awk '{print $4}')
+echo -e "${YLW}The packages will be installed in ${ACTIVE_ENV}${NC}"
 
+echo -e "${YLW}Install the C++ library${NC}"
 rm -rf build; mkdir build; cd build;
 cmake -DCMAKE_INSTALL_PREFIX=$ACTIVE_ENV ..
 make install
 
+echo -e "${YLW}Build and run tests${NC}"
 cmake -DBUILD_TESTS=ON ..
 make run_tests
 
+echo -e "${YLW}Install the Python package${NC}"
 cd ../fastscape-python/
 pip uninstall fastscape --yes
 pip install .
