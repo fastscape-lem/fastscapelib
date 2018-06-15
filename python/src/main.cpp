fc24c0450ab6f57d40758c061a23b8abfd072932
Benoit Bovy <benbovy@gmail.com>
2018-06-15 17:05:30 +0200

    fix Python bindings compilation

--- python/src/main.cpp
+++ python/src/main.cpp
@@ -7,6 +7,7 @@
 #include "pybind11/pybind11.h"
 #define FORCE_IMPORT_ARRAY
 #include "xtensor-python/pytensor.hpp"
+#include "xtensor-python/pyarray.hpp"
 
 #include "fastscapelib/utils.hpp"
 #include "fastscapelib/fastscapelib.hpp"
