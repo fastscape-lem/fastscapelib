f14e21975d72ea4fa028d20b503a0ba2c20a9431
gcordonnier <gllme.cordonnier@gmail.com>
2018-06-27 11:04:36 +0200

    add paper notebooks

--- benchmarks/jail.cpp
+++ benchmarks/jail.cpp
@@ -6,8 +6,8 @@
 #include "examples.hpp"
 
 
-template <fs::ConnectType connect, class Elev_T, class Active_T>
-void fastscape(Elev_T& elevation, const Active_T& active_nodes)
+template <fs::ConnectType connect, class Elev_T, class Angle_T, class Active_T>
+void fastscape(Elev_T& elevation, Angle_T& angles, const Active_T& active_nodes)
 {
 	auto shape = elevation.shape();
 	int nrows = (int)shape[0];
@@ -26,7 +26,7 @@ void fastscape(Elev_T& elevation, const Active_T& active_nodes)
 	xt::xtensor<double, 1> area(shape1D);
 	xt::xtensor<double, 1> erosion(shape1D);
 
-	int num_iter = 10;
+	int num_iter = 3;
 
 	for (int s = 0; s < num_iter; ++s)
 	{
@@ -38,7 +38,7 @@ void fastscape(Elev_T& elevation, const Active_T& active_nodes)
 		fastscapelib::compute_donors(ndonors, donors, receivers);
 		fastscapelib::compute_stack(stack, ndonors, donors, receivers);
 
-		fastscapelib::correct_flowrouting<fs::BasinAlgo::Boruvka, fs::ConnectType::Sloped>(bg, basins, receivers, dist2receivers,
+		fastscapelib::correct_flowrouting<fs::BasinAlgo::Boruvka, connect>(bg, basins, receivers, dist2receivers,
 			ndonors, donors, stack, active_nodes, elevation, dx, dy);
 
 		if (s != num_iter - 1)
@@ -46,13 +46,20 @@ void fastscape(Elev_T& elevation, const Active_T& active_nodes)
 			area = xt::ones<index_t>({ nrows*ncols }) * dx*dy;
 			fs::compute_drainage_area(area, stack, receivers);
 			fs::erode_spower(erosion, elevation, stack, receivers, dist2receivers, area,
-				7.0e-4f, 0.4f, 1.0f, 10.0f, 1.0e-4f);
+				7.0e-4f, 0.4f, 1.0f, 1000.0f, 1.0e-4f);
 
 			for (size_t k = 0; k < nrows*ncols; ++k)
 				elevation(k) = elevation(k) - erosion(k);
 		}
-		
+	}
 
+	for (int i = 0; i < shape1D[0]; ++i)
+	{
+		auto rcv = receivers[i];
+		auto x = rcv % ncols - i % ncols;
+		auto y = rcv / ncols - i / ncols;
+
+		angles[i] = std::atan2(double(y), double(x));
 	}
 }
 
@@ -68,24 +75,27 @@ void example_jail()
 
 	for (int y = 0; y<nrows; ++y)
 		for (int x = 0; x < ncols; ++x)
-			elevation(y, x) = 0.5 * double(std::abs(x - (ncols / 2)) + std::abs(y - (nrows / 2)));
+			elevation(y, x) = (double)std::max( std::abs(x - (ncols / 2)), std::abs(y - (nrows / 2)));
 
 	elevation(0, ncols / 2) = 0.0;
 	
 	xt::xtensor<bool, 2> active_nodes = xt::ones<bool>(elevation.shape());
 	active_nodes(0, ncols / 2) = false;
 
+	xt::xtensor<double, 2> angles(shape);
+
 	xt::xtensor<double, 2> elev_simple = elevation;
-	fastscape<fs::ConnectType::Simple>(elev_simple, active_nodes);
+	fastscape<fs::ConnectType::Simple>(elev_simple, angles, active_nodes);
 	xt::xtensor<double, 2> elev_carved = elevation;
-	fastscape<fs::ConnectType::Carved>(elev_carved, active_nodes);
+	fastscape<fs::ConnectType::Carved>(elev_carved, angles, active_nodes);
 	xt::xtensor<double, 2> elev_sloped = elevation;
-	fastscape<fs::ConnectType::Sloped>(elev_sloped, active_nodes);
+	fastscape<fs::ConnectType::Sloped>(elev_sloped, angles, active_nodes);
 
 
 	dbg_out("results/jail/elevation-", 0, elevation, shape);
 	dbg_out("results/jail/simple-", 0, elev_simple, shape);
 	dbg_out("results/jail/carved-", 0, elev_carved, shape);
 	dbg_out("results/jail/sloped-", 0, elev_sloped, shape);
+	dbg_out("results/jail/angles-", 0, angles, shape);
 
 }
