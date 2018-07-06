1564383ade80ef5e856561287bb59d53fe6440ca
Benoit Bovy <benbovy@gmail.com>
2018-07-06 09:58:32 +0200

    refactor tests + add test for arbitrary limitation of erosion

--- test/test_bedrock_channel.cpp
+++ test/test_bedrock_channel.cpp
@@ -17,26 +17,43 @@ namespace fs = fastscapelib;
 
 
 /**
- * Get slope of 1-d bedrock channel profile at steady state from
- * numerical simulation of uplift vs erosion using the stream-power
- * law.
+ * Setup for the evolution of a 1-d bedrock channel profile as a
+ * result of uplift vs. erosion using the Stream-Power law.
  *
- * Drainage area is estimated using Hack's law.
+ * Drainage area along the profile is estimated using Hack's law.
  */
-auto get_steady_slope_numerical(double k_coef,
-                                double m_exp,
-                                double n_exp,
-                                double u_rate,
-                                double hack_coef,
-                                double hack_exp,
-                                int nnodes,
-                                double spacing,
-                                double x0,
-                                double dt,
-                                double tolerance) -> xt::xtensor<double, 1>
+struct ChannelProfile1D
 {
+    double k_coef = 1e-3;
+    double m_exp = 0.5;
+    double n_exp = 1;
+
+    double u_rate = 1e-3;
+
+    int nnodes = 101;
+    double spacing = 300.;
+    double x0 = 300.;
+
+    double hack_coef = 6.69;
+    double hack_exp = 1.67;
+
+    index_t n_corr = 0;
+
+    ChannelProfile1D()
+    {
+        // fixed boundary (outlet)
+        receivers(0) = 0;
+        dist2receivers(0) = 0.;
+        uplift(0) = 0.;
+    }
+
+    xt::xtensor<double, 1> get_steady_slope_numerical(double dt, double tolerance);
+    xt::xtensor<double, 1> get_steady_slope_analytical(void);
+
+    private:
     double length = (nnodes - 1) * spacing;
-    auto x = xt::linspace<double>(length + x0, x0, static_cast<size_t>(nnodes));
+    xt::xtensor<double, 1> x = xt::linspace<double>(length + x0, x0,
+                                                    static_cast<size_t>(nnodes));
 
     xt::xtensor<double, 1> drainage_area = hack_coef * xt::pow(x, hack_exp);
     xt::xtensor<double, 1> elevation = (x - (length + x0)) * 0.001;
@@ -47,12 +64,16 @@ auto get_steady_slope_numerical(double k_coef,
 
     xt::xtensor<double, 1> uplift = xt::ones<double>({nnodes}) * u_rate;
     xt::xtensor<double, 1> erosion = xt::zeros<double>({nnodes});
+};
 
-    // fixed boundary (outlet)
-    receivers(0) = 0;
-    dist2receivers(0) = 0.;
-    uplift(0) = 0.;
 
+/**
+ * Get slope along the channel profile from the numerical solution at
+ * steady state.
+ */
+xt::xtensor<double, 1> ChannelProfile1D::get_steady_slope_numerical(double dt,
+                                                                    double tolerance)
+{
     // run model until steady state is reached
     double elevation_diff = std::numeric_limits<double>::max();
 
@@ -61,10 +82,9 @@ auto get_steady_slope_numerical(double k_coef,
         xt::xtensor<double, 1> elevation_prev = elevation;
         elevation += uplift * dt;
 
-        index_t n_corr = fs::erode_stream_power(
-            erosion, elevation, stack,
-            receivers, dist2receivers, drainage_area,
-            k_coef, m_exp, n_exp, dt, tolerance);
+        n_corr += fs::erode_stream_power(erosion, elevation, stack,
+                                         receivers, dist2receivers, drainage_area,
+                                         k_coef, m_exp, n_exp, dt, tolerance);
 
         elevation -= erosion;
 
@@ -82,28 +102,12 @@ auto get_steady_slope_numerical(double k_coef,
 
 
 /**
- * Get slope of 1-d bedrock channel profile at steady state from
- * analytical solution of uplift vs. erosion using the stream-power
- * law.
- *
- * Drainage area is estimated using Hack's law.
+ * Get slope along the channel profile from the analytical solution at
+ * steady state.
  */
-auto get_steady_slope_analytical(double k_coef,
-                                 double m_exp,
-                                 double n_exp,
-                                 double u_rate,
-                                 double hack_coef,
-                                 double hack_exp,
-                                 int nnodes,
-                                 double spacing,
-                                 double x0) -> xt::xtensor<double, 1>
+xt::xtensor<double, 1> ChannelProfile1D::get_steady_slope_analytical(void)
 {
-    double length = (nnodes - 1) * spacing;
-    auto x = xt::linspace<double>(length + x0, x0, static_cast<size_t>(nnodes));
-
-    auto drainage_area = hack_coef * xt::pow(x, hack_exp);
-
-    // exclude 1st node for comparison with numerical solution
+    // exclude 1st node for proper comparison with the numerical solution
     auto drainage_area_ = xt::view(drainage_area, xt::range(1, _));
 
     xt::xtensor<double, 1> slope = (std::pow(u_rate / k_coef, 1. / n_exp) *
@@ -115,54 +119,50 @@ auto get_steady_slope_analytical(double k_coef,
 
 TEST(bedrock_channel, erode_stream_power)
 {
-    // Test numerical vs. analytical solution of the slope of a 1-d
-    // river profile at steady state.
-    // Test for multiple n_exp values
-    // TODO: add test for n_exp < 1 (not working currently)
-    double k_coef = 1e-3;
-    double m_exp = 0.5;
     std::array<double, 3> n_exp_vals {1., 2., 4.};
-
-    double u_rate = 0.001;
-
-    int nnodes = 101;
-    double spacing = 300.;
-    double x0 = 300.;
-
-    double hack_coef = 6.69;
-    double hack_exp = 1.67;
-
-    double dt = 1e4;
-    double tolerance = 1e-3;
 
     for (const auto& n_exp : n_exp_vals)
     {
-        SCOPED_TRACE("steady-state analytical 1-d with n=" + std::to_string(n_exp));
+        SCOPED_TRACE("test against analytical solution of slope "
+                     "along a 1-d profile at steady-state for n_exp = " +
+                     std::to_string(n_exp));
 
-        auto slope_n = get_steady_slope_numerical(k_coef, m_exp, n_exp,
-                                                  u_rate, hack_coef, hack_exp,
-                                                  nnodes, spacing, x0,
-                                                  dt, tolerance);
+        auto profile_1d = ChannelProfile1D();
+        profile_1d.n_exp = n_exp;
 
-        auto slope_a = get_steady_slope_analytical(k_coef, m_exp, n_exp,
-                                                   u_rate, hack_coef, hack_exp,
-                                                   nnodes, spacing, x0);
+        auto slope_n = profile_1d.get_steady_slope_numerical(1e4, 1e-3);
+        auto slope_a = profile_1d.get_steady_slope_analytical();
 
         EXPECT_TRUE(xt::allclose(slope_n, slope_a, 1e-5, 1e-5));
+        EXPECT_TRUE(profile_1d.n_corr == 0);
     }
 
-    // TODO: Test arbitrary limitation of erosion
-
-    // Test on a tiny (2x2) 2-d square grid with a planar surface
-    // tilted in y (rows) and with all outlets on the 1st row.
     {
-        SCOPED_TRACE("simple 2-d test");
+        SCOPED_TRACE("test arbitrary limitation of erosion");
+
+        auto profile_1d = ChannelProfile1D();
+        profile_1d.n_exp = 0.8;
+
+        auto slope_n = profile_1d.get_steady_slope_numerical(1e4, 1e-3);
+
+        EXPECT_TRUE(profile_1d.n_corr > 0);
+    }
+
+    {
+        SCOPED_TRACE("test on a tiny (2x2) 2-d square grid "
+                     "with a planar surface tilted in y (rows)");
+
+        double k_coef = 1e-3;
+        double m_exp = 0.5;
+        double n_exp = 1.;
+
+        double dy = 300.;
 
         xt::xtensor<index_t, 1> receivers {0, 1, 0, 1};
-        xt::xtensor<double, 1> dist2receivers = {0., 0., spacing, spacing};
+        xt::xtensor<double, 1> dist2receivers = {0., 0., dy, dy};
         xt::xtensor<index_t, 1> stack {0, 2, 1, 3};
 
-        double a = spacing * spacing;
+        double a = dy * dy;
         xt::xtensor<double, 2> drainage_area {{2*a, 2*a}, {a, a}};
 
         double h = 1.;
@@ -170,15 +170,15 @@ TEST(bedrock_channel, erode_stream_power)
 
         xt::xtensor<double, 2> erosion = xt::empty<double>({2, 2});
 
-        double n_exp = 1.;
         double dt = 1.;  // use small time step (compare with explicit scheme)
+        double tolerance = 1e-3;
 
         index_t n_corr = fs::erode_stream_power(
             erosion, elevation, stack,
             receivers, dist2receivers, drainage_area,
             k_coef, m_exp, n_exp, dt, tolerance);
 
-        double slope = h / spacing;
+        double slope = h / dy;
         double err = dt * k_coef * std::pow(a, m_exp) * std::pow(slope, n_exp);
         xt::xtensor<double, 2> expected_erosion = {{0., 0.}, {err, err}};
 
