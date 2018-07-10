9c6b31436343df2e43123502980b72fc5f3ec9d6
Guillaume Cordonnier <guillaume.cordonnier@inria.fr>
2018-07-10 16:41:51 +0200

    fixed previous fix on drainage area...

--- include/fastscapelib/flow_routing.hpp
+++ include/fastscapelib/flow_routing.hpp
@@ -264,9 +264,10 @@ void compute_drainage_area_impl(D&& drainage_area,
 {
     // reset drainage area values (must use a view to prevent resizing
     // drainage_area to 0-d when cell_area is 0-d!)
-	// gc: flatten returns a view.
-    auto drainage_area_flat = xt::flatten(drainage_area);
+    auto drainage_area_ = xt::view(drainage_area, xt::all());
+    drainage_area_ = cell_area;
 
+    auto drainage_area_flat = xt::flatten(drainage_area_);
 
     for(auto inode=stack.crbegin(); inode!=stack.crend(); ++inode)
     {
