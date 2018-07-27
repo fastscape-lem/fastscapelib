701dd7eb7aa6c5199af38bd5e091eed940a7c5cd
Benoit Bovy <benbovy@gmail.com>
2018-07-27 15:45:16 +0200

    version control emacs dir-locals (style guide + ide setup)

---
+++ .dir-locals.el
@@ -0,0 +1,20 @@
+;;; Project-wide Emacs settings
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (c-file-style . "bsd")
+  (cmake-ide-cmake-opts . "-DBUILD_TESTS=ON -DBUILD_PYTHON_MODULE=ON"))
+ (c-mode
+  (mode . c++)
+  (flycheck-gcc-language-standard . "c++14")
+  (flycheck-clang-language-standard . "c++14")
+  (flycheck-clang-standard-library . "libc++")
+  (indent-tabs-mode . nil)
+  (c-basic-offset . 4))
+ (c++-mode
+  (flycheck-gcc-language-standard . "c++14")
+  (flycheck-clang-language-standard . "c++14")
+  (flycheck-clang-standard-library . "libc++")
+  (indent-tabs-mode . nil)
+  (c-basic-offset . 4)))
