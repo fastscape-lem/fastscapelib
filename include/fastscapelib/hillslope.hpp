6db957efa3713883e2f93bf4a0fa4761c9486394
Benoit Bovy <benbovy@gmail.com>
2018-08-03 16:37:01 +0200

    fix solve_tridiagonal

--- include/fastscapelib/hillslope.hpp
+++ include/fastscapelib/hillslope.hpp
@@ -7,6 +7,8 @@
 
 #include "xtensor/xbuilder.hpp"
 
+#include "fastscapelib/utils.hpp"
+
 
 namespace fastscapelib
 {
@@ -18,11 +20,11 @@ namespace detail
 /*
  * Solve tri-diagonal system of equations using Thomas' algorithm (TDMA).
  */
-template<class L, class D, class U, class V, class R>
-auto solve_tridiagonal(const L&& lower,
-                       const D&& diag,
-                       const U&& upper,
-                       const V&& vec) -> R
+template<class L, class D, class U, class V>
+auto solve_tridiagonal(L&& lower,
+                       D&& diag,
+                       U&& upper,
+                       V&& vec)
 {
     auto n = vec.size();
 
@@ -31,28 +33,28 @@ auto solve_tridiagonal(const L&& lower,
 
     if (diag(0) == 0)
     {
-        throw std::runtime_error("found singular tri-diagonal matrix");
+        throw std::runtime_error("division by zero while solving tri-diagonal system");
     }
 
     auto bet = diag(0);
     result(0) = vec(0) / bet;
 
-    for (auto i=1; i<n; ++i)
+    for (index_t i=1; i<n; ++i)
     {
-        gam(i) = upper(i - 1) / bet;
+        gam(i) = upper(i-1) / bet;
         bet = diag(i) - lower(i) * gam(i);
 
         if (bet == 0)
         {
-            throw std::runtime_error("found singular tri-diagonal matrix");
+            throw std::runtime_error("division by zero while solving tri-diagonal system");
         }
 
-        result(i) = (vec(i) - lower(i) * result(i - 1)) / bet;
+        result(i) = (vec(i) - lower(i) * result(i-1)) / bet;
     }
 
-    for (auto i=n - 2; i>-1; --i)
+    for (index_t i=n-2; i>-1; --i)
     {
-        result(i) = result(i) - gam(i + 1) * result(i + 1);
+        result(i) -= gam(i+1) * result(i+1);
     }
 
     return result;
