916b50692a621e30002c89e78860026f026f41fd
Benoit Bovy <benbovy@gmail.com>
2018-08-23 11:03:03 +0200

    print version in doc build log

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,9 +15,11 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+from __future__ import print_function
 import os
-import subprocess
 import platform
+import re
+import subprocess
 
 import sphinx_rtd_theme
 
@@ -70,6 +72,7 @@ def get_version():
 
 
 _version_str = get_version()
+print("building doc for fastscapelib version ", _version_str)
 
 # The short X.Y version
 version = _version_str
