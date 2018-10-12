dcab7e6dce41156db951e4f8a6c9c1682d5a2dd5
Benoit Bovy <benbovy@gmail.com>
2018-10-12 17:49:51 +0200

    temp fix for python bindings (remove when xtensor 0.18 is out)

--- python/src/main.cpp
+++ python/src/main.cpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 
 #include "pybind11/pybind11.h"
+#include "xtl/xmeta_utils.hpp"   // TODO: remove when xtensor 0.18 is released
 #define FORCE_IMPORT_ARRAY
 #include "xtensor-python/pytensor.hpp"
 #include "xtensor-python/pyarray.hpp"
