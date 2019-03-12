b77b9006c85186b3bd1af8d657994969143746f9
Benoit Bovy <benbovy@gmail.com>
2019-03-12 14:36:34 +0100

    clean-up tests warnings

--- test/test_grid.cpp
+++ test/test_grid.cpp
@@ -19,13 +19,13 @@ TEST(grid, edge_nodes_status)
     {
         SCOPED_TRACE("initializer constructor");
 
-        fs::edge_nodes_status es {fs::node_status::fixed_value_boundary,
-                                  fs::node_status::fixed_gradient_boundary};
+        fs::edge_nodes_status es2 {fs::node_status::fixed_value_boundary,
+                                   fs::node_status::fixed_gradient_boundary};
 
-        EXPECT_EQ(es.left, fs::node_status::fixed_value_boundary);
-        EXPECT_EQ(es.right, fs::node_status::fixed_gradient_boundary);
+        EXPECT_EQ(es2.left, fs::node_status::fixed_value_boundary);
+        EXPECT_EQ(es2.right, fs::node_status::fixed_gradient_boundary);
 
-        EXPECT_THROW(fs::edge_nodes_status es2 {fs::node_status::core},
+        EXPECT_THROW(fs::edge_nodes_status es3 {fs::node_status::core},
                      std::invalid_argument);
     }
 }
@@ -46,17 +46,17 @@ TEST(grid, border_nodes_status)
     {
         SCOPED_TRACE("initializer constructor");
 
-        fs::border_nodes_status bs {fs::node_status::fixed_value_boundary,
-                                    fs::node_status::looped_boundary,
-                                    fs::node_status::fixed_value_boundary,
-                                    fs::node_status::looped_boundary};
+        fs::border_nodes_status bs2 {fs::node_status::fixed_value_boundary,
+                                     fs::node_status::looped_boundary,
+                                     fs::node_status::fixed_value_boundary,
+                                     fs::node_status::looped_boundary};
 
-        EXPECT_EQ(bs.top, fs::node_status::fixed_value_boundary);
-        EXPECT_EQ(bs.right, fs::node_status::looped_boundary);
-        EXPECT_EQ(bs.bottom, fs::node_status::fixed_value_boundary);
-        EXPECT_EQ(bs.left, fs::node_status::looped_boundary);
+        EXPECT_EQ(bs2.top, fs::node_status::fixed_value_boundary);
+        EXPECT_EQ(bs2.right, fs::node_status::looped_boundary);
+        EXPECT_EQ(bs2.bottom, fs::node_status::fixed_value_boundary);
+        EXPECT_EQ(bs2.left, fs::node_status::looped_boundary);
 
-        EXPECT_THROW(fs::border_nodes_status bs2 {fs::node_status::core},
+        EXPECT_THROW(fs::border_nodes_status bs3 {fs::node_status::core},
                      std::invalid_argument);
     }
 }
@@ -64,11 +64,11 @@ TEST(grid, border_nodes_status)
 TEST(grid, profile_grid_constructor)
 {
     auto es = fs::edge_nodes_status(fs::node_status::fixed_value_boundary);
-    auto g = fs::profile_grid(10, 2, es);
+    auto g = fs::profile_grid(10, 2.0, es);
 
-    EXPECT_EQ(g.size(), 10);
-    EXPECT_EQ(g.spacing(), 2);
-    EXPECT_EQ(g.status_at_nodes().size(), 10);
+    EXPECT_EQ(g.size(), size_t(10));
+    EXPECT_EQ(g.spacing(), 2.0);
+    EXPECT_EQ(g.status_at_nodes().size(), size_t(10));
     EXPECT_EQ(g.status_at_nodes()(0), fs::node_status::fixed_value_boundary);
     EXPECT_EQ(g.status_at_nodes()(5), fs::node_status::core);
     EXPECT_EQ(g.status_at_nodes()(9), fs::node_status::fixed_value_boundary);
@@ -76,56 +76,56 @@ TEST(grid, profile_grid_constructor)
     {
         SCOPED_TRACE("test status_at_nodes parameter");
 
-        auto g = fs::profile_grid(10, 2, es,
-                                  {{5, fs::node_status::fixed_value_boundary}});
+        auto g2 = fs::profile_grid(10, 2.0, es,
+                                   {{5, fs::node_status::fixed_value_boundary}});
 
-        EXPECT_EQ(g.status_at_nodes()(5), fs::node_status::fixed_value_boundary);
+        EXPECT_EQ(g2.status_at_nodes()(5), fs::node_status::fixed_value_boundary);
     }
 
     {
         SCOPED_TRACE("test invalid looped boundary status");
 
-        fs::edge_nodes_status es {fs::node_status::fixed_value_boundary,
-                                  fs::node_status::looped_boundary};
+        fs::edge_nodes_status es2 {fs::node_status::fixed_value_boundary,
+                                   fs::node_status::looped_boundary};
 
-        EXPECT_THROW(fs::profile_grid(10, 2, es), std::invalid_argument);
+        EXPECT_THROW(fs::profile_grid(10, 2.0, es2), std::invalid_argument);
     }
 }
 
 TEST(grid, profile_grid_neighbors)
 {
     auto es = fs::edge_nodes_status(fs::node_status::fixed_value_boundary);
-    auto g = fs::profile_grid(10, 2, es);
+    auto g = fs::profile_grid(10, 2.0, es);
 
     auto& n0 = g.neighbors(0);
-    EXPECT_EQ(n0.size(), 1);
-    EXPECT_EQ(n0[0].idx, 1);
-    EXPECT_EQ(n0[0].distance, 2);
+    EXPECT_EQ(n0.size(), size_t(1));
+    EXPECT_EQ(n0[0].idx, size_t(1));
+    EXPECT_EQ(n0[0].distance, 2.0);
     EXPECT_EQ(n0[0].status, fs::node_status::core);
 
     auto& n9 = g.neighbors(9);
-    EXPECT_EQ(n9.size(), 1);
-    EXPECT_EQ(n9[0].idx, 8);
+    EXPECT_EQ(n9.size(), size_t(1));
+    EXPECT_EQ(n9[0].idx, size_t(8));
 
     auto& n5 = g.neighbors(5);
-    EXPECT_EQ(n5.size(), 2);
-    EXPECT_EQ(n5[0].idx, 4);
-    EXPECT_EQ(n5[1].idx, 6);
+    EXPECT_EQ(n5.size(), size_t(2));
+    EXPECT_EQ(n5[0].idx, size_t(4));
+    EXPECT_EQ(n5[1].idx, size_t(6));
 
     {
         SCOPED_TRACE("test looped boundary neighbors");
 
-        auto es = fs::edge_nodes_status(fs::node_status::looped_boundary);
-        auto g = fs::profile_grid(10, 2, es);
+        auto es2 = fs::edge_nodes_status(fs::node_status::looped_boundary);
+        auto g2 = fs::profile_grid(10, 2.0, es2);
 
-        auto& n0 = g.neighbors(0);
-        EXPECT_EQ(n0.size(), 2);
-        EXPECT_EQ(n0[0].idx, 9);
-        EXPECT_EQ(n0[1].idx, 1);
+        auto& n20 = g2.neighbors(0);
+        EXPECT_EQ(n20.size(), size_t(2));
+        EXPECT_EQ(n20[0].idx, size_t(9));
+        EXPECT_EQ(n20[1].idx, size_t(1));
 
-        auto& n9 = g.neighbors(9);
-        EXPECT_EQ(n9.size(), 2);
-        EXPECT_EQ(n9[0].idx, 8);
-        EXPECT_EQ(n9[1].idx, 0);
+        auto& n29 = g2.neighbors(9);
+        EXPECT_EQ(n29.size(), size_t(2));
+        EXPECT_EQ(n29[0].idx, size_t(8));
+        EXPECT_EQ(n29[1].idx, size_t(0));
     }
 }
