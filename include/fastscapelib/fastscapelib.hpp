763ae7f6be5f847f32320658a6465783e37a3397
Benoit Bovy <benbovy@gmail.com>
2022-03-08 09:15:54 +0100

    add missing grid.hpp in header bundle

--- include/fastscapelib/fastscapelib.hpp
+++ include/fastscapelib/fastscapelib.hpp
@@ -13,6 +13,7 @@
 #include "fastscapelib/flow_graph.hpp"
 #include "fastscapelib/flow_router.hpp"
 #include "fastscapelib/sink_resolver.hpp"
+#include "fastscapelib/grid.hpp"
 #include "fastscapelib/structured_grid.hpp"
 #include "fastscapelib/profile_grid.hpp"
 #include "fastscapelib/raster_grid.hpp"
