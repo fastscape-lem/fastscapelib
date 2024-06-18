b3aadb3dd64210d34933a95440a59bce97c977a1
Benoit Bovy <benbovy@gmail.com>
2024-06-18 16:27:50 +0200

    wip implement neighbors

--- include/fastscapelib/grid/healpix_grid.hpp
+++ include/fastscapelib/grid/healpix_grid.hpp
@@ -9,9 +9,11 @@
 #include <xtensor/xbroadcast.hpp>
 
 #include "fastscapelib/grid/base.hpp"
+#include "fastscapelib/utils/xtensor_containers.hpp"
 
 #include <math.h>
 #include <memory>
+#include <vector>
 
 
 namespace fastscapelib
@@ -47,7 +49,7 @@ namespace fastscapelib
      * @tparam S The container selector for data array members.
      * @tparam T The integer type used to store the HEALPix grid node indices.
      */
-    template <class S, class T = int>
+    template <class S = xt_selector, class T = int>
     class healpix_grid : public grid<healpix_grid<S, T>>
     {
     public:
@@ -67,6 +69,8 @@ namespace fastscapelib
 
         healpix_grid(T nside, double radius = 6.371e6);
 
+        // TODO: factory calculating nside from a given approx. cell area.
+
         T nside() const;
 
     protected:
@@ -77,6 +81,15 @@ namespace fastscapelib
         size_type m_size;
         double m_radius;
         double m_node_area;
+
+        using neighbors_distances_impl_type = typename base_type::neighbors_distances_impl_type;
+        using neighbors_indices_impl_type = typename base_type::neighbors_indices_impl_type;
+
+        std::vector<size_type> m_neighbors_count;
+        std::vector<neighbors_indices_impl_type> m_neighbors_indices;
+        std::vector<neighbors_distances_impl_type> m_neighbors_distances;
+
+        void set_neighbors();
 
         inline container_type nodes_areas_impl() const;
         inline grid_data_type nodes_areas_impl(const size_type& idx) const noexcept;
@@ -97,15 +110,49 @@ namespace fastscapelib
      */
     template <class S, class T>
     healpix_grid<S, T>::healpix_grid(T nside, double radius)
-        : m_radius(radius)
+        : base_type(0)
+        , m_radius(radius)
     {
         m_healpix_obj_ptr = std::make_unique<healpix_type>(nside, Healpix_Ordering_Scheme::NEST);
 
         m_size = m_healpix_obj_ptr->Npix();
         m_shape = { static_cast<typename shape_type::value_type>(m_size) };
         m_node_area = 4.0 * M_PI * m_radius * m_radius / m_size;
+
+        set_neighbors();
     }
     //@}
+
+    template <class S, class T>
+    void healpix_grid<S, T>::set_neighbors()
+    {
+        m_neighbors_count.resize(m_size);
+        m_neighbors_indices.resize(m_size);
+        m_neighbors_distances.resize(m_size);
+
+        std::array<T, 8> temp_neighbors_indices;
+
+        for (size_type inode = 0; inode < m_size; inode++)
+        {
+            size_type neighbors_count = 0;
+            auto pointing = m_healpix_obj_ptr->pix2ang(inode);
+
+            m_healpix_obj_ptr->neighbors(inode, temp_neighbors_indices);
+
+            for (size_type k = 1; k < 8; ++k)
+            {
+                size_type ineighbor = temp_neighbors_indices[k];
+
+                if (ineighbor > -1)
+                {
+                    m_neighbors_indices[inode][neighbors_count] = ineighbor;
+                    neighbors_count++;
+                }
+            }
+
+            m_neighbors_count[inode] = neighbors_count;
+        }
+    }
 
     template <class S, class T>
     auto healpix_grid<S, T>::nside() const -> T
--- test/test_healpix_grid.cpp
+++ test/test_healpix_grid.cpp
@@ -1,1 +1,19 @@
 #include "fastscapelib/grid/healpix_grid.hpp"
+
+#include "gtest/gtest.h"
+
+
+namespace fs = fastscapelib;
+
+
+namespace fastscapelib
+{
+    namespace testing
+    {
+
+        TEST(healpix_grid, ctor)
+        {
+            auto grid = fs::healpix_grid<>(25);
+        }
+    }
+}
