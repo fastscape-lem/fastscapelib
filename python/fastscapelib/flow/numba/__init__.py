e830ac8e82f611652a7b3ee29797d55d482845c2
Benoit Bovy <benbovy@gmail.com>
2025-06-23 10:52:45 +0200

    fix mypy

--- python/fastscapelib/flow/numba/__init__.py
+++ python/fastscapelib/flow/numba/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, Callable, Iterable
 
-from _fastscapelib_py.flow import FlowGraphTraversalDir
+from _fastscapelib_py.flow import FlowGraphTraversalDir  # type: ignore[import]
 
 if TYPE_CHECKING:
     import numba as nb
